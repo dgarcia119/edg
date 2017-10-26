dcd2a1b0505f3d897cc43ee6991e765c500e532d
dgarcia119 <32489954+dgarcia119@users.noreply.github.com>
2017-10-26 18:06:46 +0200

    Production v5

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var http = require('http'); // create an http server to handle requests and response 
 http.createServer(function (req, res) { // sending a response header of 200 OK 
 res.writeHead(200, {'Content-Type': 'text/plain'}); // print out Hello World 
-res.end('Production v4\n'); // use port 8080 
+res.end('Production v5\n'); // use port 8080 
 }).listen(8080); 
 console.log('Server running on port 8080.');
 
